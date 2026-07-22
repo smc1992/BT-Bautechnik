89ca84dbfe212686b4e739b3f303ccb1a403d2f3
Simon Müller <smc1992@example.com>
2026-07-22 18:05:48 +0200

    style: custom BT Bautechnik logo on login and fix Pest tests

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <title>{{ config('app.name', 'BT Bautechnik') }}</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
@@ -15,14 +15,14 @@
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans text-gray-900 antialiased">
-        <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
-            <div>
-                <a href="/" wire:navigate>
-                    <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
+        <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gradient-to-tr from-gray-100 via-white to-gray-200 dark:from-gray-950 dark:via-gray-900 dark:to-gray-800">
+            <div class="mb-4">
+                <a href="/" wire:navigate class="transition duration-300 hover:scale-105 block">
+                    <x-application-logo class="h-28 w-auto" />
                 </a>
             </div>
 
-            <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
+            <div class="w-full sm:max-w-md mt-6 px-8 py-8 bg-white dark:bg-gray-900/60 dark:backdrop-blur-md border border-gray-200 dark:border-gray-700/30 dark:border-gray-800/80 shadow-2xl overflow-hidden sm:rounded-2xl">
                 {{ $slot }}
             </div>
         </div>

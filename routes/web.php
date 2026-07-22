c436c2bd245102982b80f94699c3a4d52aaf223c
Simon Müller <smc1992@example.com>
2026-07-22 18:04:25 +0200

    security: disable public registration and redirect home to login page

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-
-Route::view('/', 'welcome');
+Route::get('/', function () {
+    return redirect()->route('login');
+});
 
 Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
